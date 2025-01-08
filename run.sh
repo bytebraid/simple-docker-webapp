79aae9cc1a129610baa5daefd39435dceb933ff8
bytebraid <ciaran@parallaxed.net>
2025-01-08 21:04:26 +0000

    Update docker-deploy

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
-docker build -t simple-docker-webapp .
+sudo -g docker docker stop simple-docker-webapp
+sudo -g docker docker rm simple-docker-webapp
+sudo -g docker docker build -t simple-docker-webapp .
 
 # This is not production ready, Flask is a dev server
 # Should use the -u flag to specify a user other than root
-docker run -p 11000:11000 simple-docker-webapp
+sudo -g docker docker run -d -p 11000:11000 --name simple-docker-webapp simple-docker-webapp
